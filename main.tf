6c25e7722d0ddaa5b9a249bc87550aac81512c6c
Alejsilva05 <alessandro3@gmail.com>
2021-01-20 18:02:48 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,11 +4,3 @@ provider "aws" {
   version = "~> 2.0"
 }
 
-terraform {
-  backend "s3" {
-    # Lembre de trocar o bucket para o seu, não pode ser o mesmo nome
-    bucket = "descomplicando-terraform-gomex-tfstates"
-    key    = "terraform-test.tfstate"
-    region = "us-east-1"
-  }
-}
