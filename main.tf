036b758aa2d20193a40b2baeb72916df1af4d342
Alejsilva05 <alessandro3@gmail.com>
2021-01-20 17:57:22 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,14 @@
 # Configure the AWS Provider
-
 provider "aws" {
-  region     = "us-east-2"
-  access_key = "AKIA6PVKCGPZARQUMNWY"
-  secret_key = "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"
+  region  = "us-east-1"
+  version = "~> 2.0"
 }
 
-#resource "aws_s3_bucket" "b" {
-#  bucket = "my-tf-test-bucket-3234343545"
-#  acl    = "private"
-
-#  tags = {
-#    Name        = "My super bucket"
-#    Environment = "Prod"
-#  }
-
-
-#resource "aws_s3_bucket_object" "object" {
-#  bucket = "${aws_s3_bucket.b.id}"
-#  key    = "hello-word.txt"
-#  source = "arquivo.txt"
-
-#  }
+terraform {
+  backend "s3" {
+    # Lembre de trocar o bucket para o seu, não pode ser o mesmo nome
+    bucket = "descomplicando-terraform-gomex-tfstates"
+    key    = "terraform-test.tfstate"
+    region = "us-east-1"
+  }
+}
