6b09884afb2e71382175fb000986a612a37ef7bd
Alejsilva05 <alessandro3@gmail.com>
2021-01-20 18:43:50 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,20 @@
+  
 provider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket-1231231231265656"
+  bucket = "my-tf-test-bucket-12312312312"
   acl    = "private"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
+}
+
+resource "aws_s3_bucket_object" "object" {
+  bucket = "${aws_s3_bucket.b.id}"
+  key    = "hello-world.txt"
+  source = "arquivo.txt"
 }
